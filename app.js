7b4b5a79ad8aeed512764e14293adbd27de9ca05
Nigo52 <103093087+Nigo52@users.noreply.github.com>
2022-09-19 11:46:28 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const port=process.env.port||80;
+const app = express();
+const router = express.Router();
+const fs = require("fs");
+
+app.listen(port);
+console.log("Web has been initialized" +" "+ port);
+
+//--------------------Routing----------------------------------//
+router.get('/',(request,response)=>
+{
+    data = fs.readFileSync("./html/main.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+router.get('/home',(request,response)=>
+{
+    data = fs.readFileSync("./html/home.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+router.get('/about',(request,response)=>
+{
+    data = fs.readFileSync("./html/about.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+router.get('/contact',(request,response)=>
+{
+    data = fs.readFileSync("./html/contact.html");
+    pageContent = data.toString();
+    response.send(pageContent);
+});
+//------------------------------------------------------------//
+app.use('/', router);
+app.use('/home', router);
+app.use('/about', router);
+app.use('/contact', router);
+
+//--------------------Launch----------------------------------//
